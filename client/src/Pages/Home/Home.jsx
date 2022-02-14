1ddda8f52d7f042607eba6e8e66adf18af4352ac
Chan Sze Lam <chanszelam7725@gmail.com>
2022-02-14 21:44:46 +0800

    update homepage popup login form

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 import { Button, Grid, Typography } from "@mui/material";
+import LoginButton from "../../Components/UI/LoginButton.jsx";
 
 import HubBoardIcon from "../../Image/HubBoard.svg";
 import MessageIcon from "../../Image/message_google.svg";
 import TodoIcon from "../../Image/to-do-iOS.svg";
-import { CloudOutlined, LoginRounded } from "@mui/icons-material";
+import { CloudOutlined } from "@mui/icons-material";
 
 import classes from "./Home.module.css";
 
 const Home = () => {
+
   return (
     <div className={classes.fade}>
       <Grid container mt="1em">
-        <Grid xs={2} />
-        <Grid item xs={1}>
+        <Grid item xs={0} md={2} />
+        <Grid item xs={3} md={1} >
           <img src={HubBoardIcon} height="75px" />
         </Grid>
-        <Grid item container xs={1} alignItems="center">
+        <Grid item container xs={3} md={1} alignItems="center">
           <Typography fontSize="1em">HubBoard</Typography>
         </Grid>
         <Grid
           item
           container
-          xs={6}
+          xs={6} 
+          md={6}
           justifyContent="flex-end"
           alignItems="center"
         >
-          <Button
-            sx={{ maxHeight: "50%" }}
-            size="medium"
-            startIcon={<LoginRounded />}
-          >
-            Log in
-          </Button>
+          <LoginButton/>
         </Grid>
-        <Grid xs={1} />
+        <Grid item xs={0} md={2} />
       </Grid>
-      <Grid container mt="5em">
-        <Grid xs={2} />
-        <Grid item container xs={6}>
-          <Grid item xs={12}>
+      <Grid container mt="3em" spacing={8}>
+        <Grid item xs={0} md={2}/>
+        <Grid item container xs={12} md={6} >
+          <Grid item xs={12} >
             <Typography variant="h1" fontSize="4em">
               Welcome to <span style={{ color: "orange" }}>HubBoard</span>
             </Typography>
@@ -50,18 +47,18 @@ const Home = () => {
             </Typography>
           </Grid>
         </Grid>
-          <Grid item container xs={3}>
-          <Grid item xs={6} textAlign="center">
-            <img src={MessageIcon} height="100px"/>
+        <Grid item container xs={12} md={4}>
+          <Grid item xs={12} md={6} textAlign="center">
+            <img src={MessageIcon} height="100px" />
           </Grid>
-          <Grid item xs={6} textAlign="center">
-            <img src={TodoIcon} height="100px"/>
+          <Grid item xs={12} md={6} textAlign="center">
+            <img src={TodoIcon} height="100px" />
           </Grid>
-          <Grid item xs={12} textAlign="center">
-            <CloudOutlined sx={{fontSize: "100px"}}/>
+          <Grid item xs={12} md={12} textAlign="center">
+            <CloudOutlined sx={{ fontSize: "100px" }} />
           </Grid>
         </Grid>
-        <Grid xs={1} />
+        <Grid item xs={1} />
       </Grid>
     </div>
   );
