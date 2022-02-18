738b1e2290228e7a1e12cca78de832d1ebed811e
johnnkp <kpng0@cse.cuhk.edu.hk>
2022-02-19 01:06:57 +0800

    Homepage part 3: FHD layout

--- client/src/Styles/theme.js
+++ client/src/Styles/theme.js
@@ -1,68 +1,80 @@
-import { createTheme } from "@mui/material/styles";
-import { orange } from "@mui/material/colors";
+import {createTheme} from "@mui/material/styles";
+import {orange} from "@mui/material/colors";
 /* Apple San Francisco Pro font */
 import SFProDisplayRegular from "./fonts/SF-Pro-Display-Regular.woff2";
+import SFProTextSemibold from "./fonts/SF-Pro-Text-Semibold.woff2";
 // import SFProDisplayMedium from "./fonts/SF-Pro-Display-Medium.woff2";
+
 // import FilsonSoftRegular from "./fonts/Filson-Soft-W03-Regular.woff2";
 
 const theme = createTheme({
-  typography: {
-    // add font-family name in array
-    fontFamily: ["sf_pro_display_regular", "Roboto"].join(","),
-    // sample code to add variant
-    // poster: {
-    //   color: "red",
-    //   fontFamily: "filson_soft_regular",
-    // },
-  },
-  palette: {
-    hOrange: {
-      main: orange[500],
-      contrastText: "#fff",
+    typography: {
+        // add font-family name in array
+        fontFamily: ["sf_pro_display_regular", "sf_pro_text_semibold", "Roboto"].join(","),
+        // sample code to add variant
+        // poster: {
+        //   color: "red",
+        //   fontFamily: "filson_soft_regular",
+        // },
     },
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides:
-        (`        
-      @font-face {
-          font-family: 'sf_pro_display_regular';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: url(${SFProDisplayRegular}) format('woff2');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }`
-      // sample code to add font-family
-      // , `
-      // @font-face {
-      //     font-family: 'filson_soft_regular';
-      //     font-style: normal;
-      //     font-display: swap;
-      //     font-weight: 400;
-      //     src: url(${FilsonSoftRegular}) format('woff2');
-      //     unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-      //   }`
-      ),
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "& label.Mui-focused": {
-            color: orange[500],
-          },
-          "& .MuiOutlinedInput-root": {
-            "&:hover fieldset": {
-              borderColor: orange[400],
-            },
-            "&.Mui-focused fieldset": {
-              borderColor: orange[500],
-            },
-          },
+    palette: {
+        hOrange: {
+            main: orange[500],
+            contrastText: "#fff",
         },
-      },
     },
-  },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides:
+                (`        
+                    @font-face {
+                        font-family: 'sf_pro_display_regular';
+                        font-style: normal;
+                        font-display: swap;
+                        font-weight: 400;
+                        src: url(${SFProDisplayRegular}) format('woff2');
+                        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+                    }
+
+                    @font-face {
+                        font-family: 'sf_pro_text_semibold';
+                        font-style: normal;
+                        font-display: swap;
+                        font-weight: normal;
+                        src: url(${SFProTextSemibold}) format('woff2');
+                        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+                    }
+		        `
+                    // sample code to add font-family
+                    // , `
+                    // @font-face {
+                    //     font-family: 'filson_soft_regular';
+                    //     font-style: normal;
+                    //     font-display: swap;
+                    //     font-weight: 400;
+                    //     src: url(${FilsonSoftRegular}) format('woff2');
+                    //     unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+                    //   }`
+                ),
+        },
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    "& label.Mui-focused": {
+                        color: orange[500],
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "&:hover fieldset": {
+                            borderColor: orange[400],
+                        },
+                        "&.Mui-focused fieldset": {
+                            borderColor: orange[500],
+                        },
+                    },
+                },
+            },
+        },
+    },
 });
 
 export default theme;
