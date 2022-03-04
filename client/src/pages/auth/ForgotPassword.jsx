cb23aa12a708e5c6c9dea5d599882512e7ecbecf
johnnkp <kpng0@cse.cuhk.edu.hk>
2022-03-04 17:11:01 +0800

    Adjust forget password page

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -3,22 +3,22 @@ import ResendEmail from "../../components/Helpers/ResendEmail"
 import axios from "axios";
 
 const ForgotPassword = () => {
-  // INFO: custom submit handler
-  const resendForgotPasswordEmail = async (values, action) => {
-    try {
-      const res = await axios.post("/api/auth/resetPasswordEmail", values);
-      return res.data;
-    } catch (err) {
-      return err.response.data;
-    }
-  };
-  
-  // INFO: operation type = 2
-  return (
-    <ResendEmail resendEmailHandler={resendForgotPasswordEmail} type={2}>
-      Forgot password email has been send, please check again your email
-    </ResendEmail>
-  );
+    // INFO: custom submit handler
+    const resendForgotPasswordEmail = async (values, action) => {
+        try {
+            const res = await axios.post("/api/auth/resetPasswordEmail", values);
+            return res.data;
+        } catch (err) {
+            return err.response.data;
+        }
+    };
+
+    // INFO: operation type = 2
+    return (
+        <ResendEmail resendEmailHandler={resendForgotPasswordEmail} type={2}>
+            Forgot password email has been sent. Please check your email again.
+        </ResendEmail>
+    );
 };
 
 export default ForgotPassword;
