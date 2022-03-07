14703b9e1c4afed54a063b6447fc2d79e6deacd0
Chan Sze Lam <chanszelam7725@gmail.com>
2022-03-08 00:16:01 +0800

    remove unnecessary import

--- client/src/pages/hubboard/main/Mainpage.jsx
+++ client/src/pages/hubboard/main/Mainpage.jsx
@@ -1,39 +1,18 @@
-import axios from "axios";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import {AppBar, Container, Toolbar} from "@mui/material";
-import { authActions } from "../../../store/slice/auth";
+import { AppBar, Container, Toolbar } from "@mui/material";
 
 const Mainpage = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const logoutHandler = async () => {
-    try {
-      const res = await axios.post("/api/user/logout");
-      if (res.data.success) {
-        dispatch(authActions.logout());
-        setTimeout(() => {
-          navigate("/");
-        }, 1000);
-      }
-    } catch (err) {
-      console.log(err.response);
-    }
-  };
   return (
     <div>
       <AppBar color="hOrange">
         <Container>
-          <Toolbar>
-
-          </Toolbar>
+          <Toolbar></Toolbar>
         </Container>
       </AppBar>
-      <Container sx={{position: "relative", top: 50}}>
+      <Container sx={{ position: "relative", top: 50 }}>
         <h1>You in mainpage</h1>
         <button onClick={logoutHandler}>Logout</button>
-	  </Container>
+      </Container>
     </div>
   );
 };
