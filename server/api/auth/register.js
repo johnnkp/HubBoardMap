c36b1de36f73a6aad18a4d4cd98c4587ebb176a4
hotomho <74140578+hotomho@users.noreply.github.com>
2022-03-08 15:53:20 +0800

    check for info validity when register

--- server/api/auth/register.js
+++ server/api/auth/register.js
@@ -24,80 +24,118 @@ const User = require('../../database/model/User');
 const mailer = require('../../lib/mailer');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const validator = require("email-validator");
 require('dotenv').config();
 
 
 router.post('/', (req, res) => {
-  const { username, email, password } = req.body;
+    const { username, email, password } = req.body;
 
-  // TODO: Check if username or email is valid
-
-  // Check if username or email is used
-  User.findOne({$or:[{username: username}, {email: email}]})
-      .then(result =>{
-            // If username or email is used
-          if (result) {
-              if (result.username === username) {
-                  res.status(409).json({
-                      success: false,
-                      error_attr : "username",
-                      message: "Username already used"
-                  });
-              } else {
-                  res.status(409).json({
-                      success: false,
-                      error_attr : "email",
-                      message: "Email already used"
-                  });
-              }
-          }
-            // If username or email is not used
-          else {
-              const verificationToken = crypto.randomBytes(20).toString('hex');
-              // Create new user in database
-              User.create({
-                  username: username,
-                  email: email,
-                  password: bcrypt.hashSync(password, Number(process.env.SALT)),
-                  verificationToken: verificationToken
-              })
-                  .then(() =>{
-                      // Send verification email
-                      mailer.sendVerificationEmail(email, verificationToken)
-                          // If email is sent
-                          .then(()=>{
-                              res.status(202).json({
-                                  success: true,
-                                  message: "Verification email sent"
-                              });
-                          })
-                          // Catch error when sending verification email
-                          .catch(err =>{
-                              console.error(err);
-                              res.status(500).json({
-                                  success: false,
-                                  message: "Internal server error"
-                              });
-                          });
-                      })
-                  // Catch error when creating user
-                  .catch(err =>{
-                      console.error(err);
-                      res.status(500).json({
-                          success: false,
-                          message: "Internal server error"
-                      });
-                  });
-            }
-          })
-      // Catch error when finding user
-      .catch(err =>{
-          console.error(err);
-          res.status(500).json({
-              success: false,
-              message: "Internal server error"
-          });
-      });
+    // Check if username, email and password is valid
+    if (!username) {
+        return res.status(409).json({
+            success: false,
+            error_attr: 'username',
+            message: 'Username is required'
+        });
+    } else if (username.length < 3) {
+        return res.status(409).json({
+            success: false,
+            error_attr: 'username',
+            message: 'Username should be of minimum 3 character'
+        });
+    } else if (!email) {
+        return res.status(409).json({
+            success: false,
+            error_attr: 'email',
+            message: 'Email is required'
+        });
+    } else if (!validator.validate(email)){
+        return res.status(409).json({
+            success: false,
+            error_attr: 'email',
+            message: 'Email is not valid'
+        });
+    } else if (!password) {
+        return res.status(409).json({
+            success: false,
+            error_attr: 'password',
+            message: 'Password is required'
+        });
+    } else if (password.length < 6) {
+        return res.status(409).json({
+            success: false,
+            error_attr: 'password',
+            message: 'Password should be of minimum 6 character'
+        });
+    } else {
+        // Check if username or email is used
+        User.findOne({$or:[{username: username}, {email: email}]})
+            .then(result =>{
+                // If username or email is used
+                if (result) {
+                    if (result.username === username) {
+                        res.status(409).json({
+                            success: false,
+                            error_attr : "username",
+                            message: "Username already used"
+                        });
+                    } else {
+                        res.status(409).json({
+                            success: false,
+                            error_attr : "email",
+                            message: "Email already used"
+                        });
+                    }
+                }
+                // If username or email is not used
+                else {
+                    const verificationToken = crypto.randomBytes(20).toString('hex');
+                    // Create new user in database
+                    User.create({
+                        username: username,
+                        email: email,
+                        password: bcrypt.hashSync(password, Number(process.env.SALT)),
+                        verificationToken: verificationToken
+                    })
+                        .then(() =>{
+                            // Send verification email
+                            mailer.sendVerificationEmail(email, verificationToken)
+                                // If email is sent
+                                .then(()=>{
+                                    res.status(202).json({
+                                        success: true,
+                                        message: "Verification email sent"
+                                    });
+                                })
+                                // Catch error when sending verification email
+                                .catch(err =>{
+                                    console.error(err);
+                                    res.status(500).json({
+                                        success: false,
+                                        message: "Internal server error"
+                                    });
+                                });
+                        })
+                        // Catch error when creating user
+                        .catch(err =>{
+                            console.error(err);
+                            res.status(500).json({
+                                success: false,
+                                message: "Internal server error"
+                            });
+                        });
+                }
+            })
+            // Catch error when finding user
+            .catch(err =>{
+                console.error(err);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal server error"
+                });
+            });
+    }
 });
 
 module.exports = router;
