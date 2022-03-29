38623d523cb3645d41d9df5a7c81706fd4ab5134
hotomho <74140578+hotomho@users.noreply.github.com>
2022-03-29 22:32:41 +0800

    test code

--- client/src/components/UI/Utils/Notification.jsx
+++ client/src/components/UI/Utils/Notification.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:3001",{
+  transports: ["websocket"]
+});
 
 const Notification = () => {
+  socket.on('notifications', notifications =>{
+    console.log(notifications);
+  });
   return (
     <IconButton sx={{ color: "white" }}>
       <Badge badgeContent={4} color="info">
