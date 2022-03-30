3bc40df93e2f1c063cadcde437da6d2c7ec8caf7
hotomho <74140578+hotomho@users.noreply.github.com>
2022-03-30 21:23:23 +0800

    login test case

--- test/api/auth/login.test.js
+++ test/api/auth/login.test.js
@@ -9,32 +9,98 @@ chai.use(chaiHttp);
 
 
 describe('POST /api/auth/login', ()=>{
-    it('correct password', function (done) {
+    it('Case: correct password', function (done) {
         chai.request(server)
             .post('/api/auth/login')
             .send({
-                username: "testac",
-                password: "testing"
+                username: "testAccount",
+                password: "password"
             })
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.a('object');
                 expect(res.body).to.have.property('success',true);
+                expect(res.header).to.have.property('set-cookie');
                 done();
             });
     });
-    it('incorrect password', function (done) {
+
+    it('Case: invalid username', function (done) {
         chai.request(server)
             .post('/api/auth/login')
             .send({
-                username: "testac",
-                password: "wrongpassword"
+                username: "1",
+                password: "Password"
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('success',false);
+                expect(res.body).to.have.property('error_code',1);
+                done();
+            });
+    });
+
+    it("Case: invalid password", function (done) {
+        chai.request(server)
+            .post('/api/auth/login')
+            .send({
+                username: "testAccount",
+                password: "1"
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('success',false);
+                expect(res.body).to.have.property('error_code',1);
+                done();
+            });
+    });
+
+    it("Case: login without verified email", function (done) {
+        chai.request(server)
+            .post('/api/auth/login')
+            .send({
+                username: "testAccount00",
+                password: "password"
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('success',false);
+                expect(res.body).to.have.property('error_code',2);
+                done();
+            });
+    });
+
+    it('Case: incorrect password', function (done) {
+        chai.request(server)
+            .post('/api/auth/login')
+            .send({
+                username: "testAccount",
+                password: "incorrectPassword"
             })
             .end((err, res) => {
                 expect(res).to.have.status(400);
                 expect(res.body).to.be.a('object');
                 expect(res.body).to.have.property('success',false);
                 expect(res.body).to.have.property('error_code',3);
+                done();
+            });
+    });
+
+    it("Case: user not found", function (done) {
+        chai.request(server)
+            .post('/api/auth/login')
+            .send({
+                username: "notFound",
+                password: "password"
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('success',false);
+                expect(res.body).to.have.property('error_code',4);
                 done();
             });
     });
