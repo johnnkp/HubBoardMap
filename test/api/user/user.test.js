1f503110710f8ed1c68dfb22cb2e7d6423aa92d2
hotomho <74140578+hotomho@users.noreply.github.com>
2022-03-31 22:17:28 +0800

    user operations test update

--- test/api/user/user.test.js
+++ test/api/user/user.test.js
@@ -25,52 +25,54 @@ before(done=>{
             done(new Error("Login failed: " + err));
         });
 })
+describe("User Operations",()=>{
+    describe('Auth Verify', ()=>{
+        it('Success',done=>{
+            chai.request(server)
+                .get('/api/user/authVerify')
+                .set('Cookie',cookie)
+                .then(res=>{
+                    expect(res).to.have.status(200);
+                    done();
+                })
+                .catch(err=>{
+                    throw err;
+                })
+        })
+    })
 
-describe('Auth Verify', ()=>{
-    it('Success',done=>{
-        chai.request(server)
-            .get('/api/user/authVerify')
-            .set('Cookie',cookie)
-            .then(res=>{
-                expect(res).to.have.status(200);
-                done();
-            })
-            .catch(err=>{
-                throw err;
-            })
+    describe('Get current user info', ()=>{
+        it('Success', done=>{
+            chai.request(server)
+                .get('/api/user/info')
+                .set('Cookie',cookie)
+                .then(res=>{
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('username',testUser.username);
+                    expect(res.body).to.have.property('email');
+                    done();
+                })
+                .catch(err=>{
+                    throw err;
+                })
+        })
+    })
+
+    describe('Get current user profile photo', ()=>{
+        it('Success', done=>{
+            chai.request(server)
+                .get('/api/user/profilePhoto/')
+                .set('Cookie',cookie)
+                .then(res=>{
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.instanceof(Buffer);
+                    done();
+                })
+                .catch(err=>{
+                    throw err;
+                })
+        })
     })
 })
 
-describe('Get current user info', ()=>{
-    it('Success', done=>{
-        chai.request(server)
-            .get('/api/user/info')
-            .set('Cookie',cookie)
-            .then(res=>{
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.property('username',testUser.username);
-                expect(res.body).to.have.property('email');
-                done();
-            })
-            .catch(err=>{
-                throw err;
-            })
-    })
-})
-
-describe('Get current user profile photo', ()=>{
-    it('Success', done=>{
-        chai.request(server)
-            .get('/api/user/profilePhoto/')
-            .set('Cookie',cookie)
-            .then(res=>{
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.instanceof(Buffer);
-                done();
-            })
-            .catch(err=>{
-                throw err;
-            })
-    })
-})
 
