f696b01244b4307271cdb4ec7ffaebc729f26399
johnnkp <22496821+johnnkp@users.noreply.github.com>
2022-05-02 16:50:17 +0800

    MailSuccess: fix typo

--- client/src/pages/auth/register/MailSuccess.jsx
+++ client/src/pages/auth/register/MailSuccess.jsx
@@ -1,3 +1,4 @@
+// Verification email sent page
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Box, Button, Stack, Typography } from "@mui/material";
@@ -12,7 +13,7 @@ const MailSuccess = () => {
     >
       <Stack direction="column">
         <Typography textAlign="center" variant="h1" fontSize="2em">
-          Verfication email send, please check you email
+          Verification email has been sent, please check your email
         </Typography>
         <Button to="/" LinkComponent={RouterLink} color="hOrange" >Return to homepage</Button>
       </Stack>
