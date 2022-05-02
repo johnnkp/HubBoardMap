24d08e8046c7b57200f0598f2c8d7a4a338419eb
johnnkp <1155144829@link.cuhk.edu.hk>
2022-05-02 15:40:28 +0800

    Document: add Redux Store Slice Driver comment

--- client/src/store/slice/index.js
+++ client/src/store/slice/index.js
@@ -1,4 +1,16 @@
-export { default as authReducer } from "./auth";
-export { default as todoListReducer } from "./todo";
-export { default as notificationReducer } from "./notification";
-export { default as commentReducer } from "./comment";
+/* PROGRAM Redux Store Slice Driver - Centralise export of Redux Store Slice
+ * PROGRAMMER: SzelamC
+ * USAGE: import {authReducer, commentReducer, notificationReducer, todoListReducer} from "<relative directory>/store/slice";
+ * VERSION 1: written 30-03-2022
+ * Revision 1.1: 02-05-2022 add code comment
+ * PURPOSE: Centralise Redux Store Slice function to simplify import package.
+ * DATA STRUCTURE:
+ * export { default as authReducer } - auth.js
+ * export { default as commentReducer } - comment.js
+ * export { default as notificationReducer } - notification.js
+ * export { default as todoListReducer } - todo.js */
+
+export {default as authReducer} from "./auth";
+export {default as commentReducer} from "./comment";
+export {default as notificationReducer} from "./notification";
+export {default as todoListReducer} from "./todo";
