94bf0f8a430b34fde07d2b3a9a7883394eb1399c
johnnkp <22496821+johnnkp@users.noreply.github.com>
2022-12-13 22:42:48 +0800

    Use MongoDB node driver when mongoose fail
    Error connecting to MongoDB: MongooseServerSelectionError: Could not connect to any servers in your MongoDB Atlas cluster. One common reason
     is that you're trying to access the database from an IP that isn't whitelisted. Make sure your current IP address is on your Atlas cluster's IP whit
    elist: https://docs.atlas.mongodb.com/security-whitelist/
    
    error: MongoNetworkError: connection <monitor> to 18.162.98.145:27017 closed
    	at Connection.onClose (C:\Users\Anaconda\Desktop\CSCI3100Project_HubBoard\node_modules\mongodb\lib\cmap\connection.js:136:19)   
    	at Socket.<anonymous> (C:\Users\Anaconda\Desktop\CSCI3100Project_HubBoard\node_modules\mongodb\lib\cmap\connection.js:63:46)    
    	at Socket.emit (node:events:526:28)
    	at TCP.<anonymous> (node:net:687:12) {
    	[Symbol(errorLabels)]: Set(1) { 'ResetPool' }
    	}

--- server/database/index.js
+++ server/database/index.js
@@ -1,22 +1,32 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to the database
 module.exports = {
     connect: () => {
-        mongoose.connect(MONGODB_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: "HubBoard"
-        });
-        mongoose.connection.on('connected', () => {
-            console.log('Connected to MongoDB')
-        });
-        mongoose.connection.on('error', (err) => {
-            console.log('Error connecting to MongoDB: ' + err)
-        });
+        try {
+            mongoose.connect(MONGODB_URI, {dbName: "HubBoard"}).catch(
+                new MongoClient
+                // Replace the following with your MongoDB deployment's connection string.
+                ("mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority")
+                    .connect()
+                    .then(() => console.log('Connected to MongoDB'))
+            );
+            mongoose.connection.on('connected', () => {
+                console.log('Connected to MongoDB')
+            });
+            mongoose.connection.on('error', (err) => {
+                console.log('Error connecting to MongoDB: ' + err)
+            });
+        } catch (e) {
+            new MongoClient
+            // Replace the following with your MongoDB deployment's connection string.
+            ("mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority")
+                .connect()
+                .then(() => console.log('Connected to MongoDB'));
+        }
     }
 }
-
